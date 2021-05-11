467dfbb253b8672b7463e8516e9aedc78dab2ad6
Christopher Silich <chris.silich@gmail.com>
2021-05-11 15:43:25 -0400

    updated template for better responsiveness

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,38 @@
 "use strict";
 
-console.log("Hello World from main.js!");
+console.log('Hello World from main.js!');
+var tl = gsap.timeline({
+  repeat: -1,
+  onRepeat: function onRepeat() {
+    if (tl.timeScale() < 10) tl.timeScale(tl.timeScale() * 1.2);
+  }
+});
+tl.to('.square', {
+  duration: 1.5,
+  ease: 'power1.inOut',
+  left: '40%',
+  top: '58%',
+  rotation: 90
+});
+tl.to('.square', {
+  duration: 1.5,
+  ease: 'power1.inOut',
+  left: '40%',
+  top: '10%',
+  rotation: 180
+});
+tl.to('.square', {
+  duration: 1.5,
+  ease: 'power1.inOut',
+  left: '10%',
+  top: '58%',
+  rotation: 270
+});
+tl.to('.square', {
+  duration: 1.5,
+  ease: 'power1.inOut',
+  left: '10%',
+  top: '10%',
+  rotation: 180
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log("Hello World from main.js!");
+console.log('Hello World from main.js!');
+
+let tl = gsap.timeline({
+	repeat: -1,
+	onRepeat() {
+		if (tl.timeScale() < 10) tl.timeScale(tl.timeScale() * 1.2);
+	},
+});
+tl.to('.square', { duration: 1.5, ease: 'power1.inOut', left: '40%', top: '58%', rotation: 90 });
+tl.to('.square', { duration: 1.5, ease: 'power1.inOut', left: '40%', top: '10%', rotation: 180 });
+tl.to('.square', { duration: 1.5, ease: 'power1.inOut', left: '10%', top: '58%', rotation: 270 });
+tl.to('.square', { duration: 1.5, ease: 'power1.inOut', left: '10%', top: '10%', rotation: 180 });
