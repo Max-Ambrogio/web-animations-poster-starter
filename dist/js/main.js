f58b6aff435def91c118be662cad52797ff619dc
Christopher Silich <chris.silich@gmail.com>
2021-05-11 15:47:04 -0400

    seizure warning

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,15 @@
 "use strict";
 
 console.log('Hello World from main.js!');
+gsap.to('.square', {
+  duration: 2,
+  delay: 10,
+  color: 'white'
+});
 var tl = gsap.timeline({
   repeat: -1,
   onRepeat: function onRepeat() {
-    if (tl.timeScale() < 10) tl.timeScale(tl.timeScale() * 1.2);
+    if (tl.timeScale() < 30) tl.timeScale(tl.timeScale() * 1.2);
   }
 });
 tl.to('.square', {
@@ -12,27 +17,27 @@ tl.to('.square', {
   ease: 'power1.inOut',
   left: '40%',
   top: '58%',
-  rotation: 90
+  rotation: '+=90'
 });
 tl.to('.square', {
   duration: 1.5,
   ease: 'power1.inOut',
   left: '40%',
   top: '10%',
-  rotation: 180
+  rotation: '+=90'
 });
 tl.to('.square', {
   duration: 1.5,
   ease: 'power1.inOut',
   left: '10%',
   top: '58%',
-  rotation: 270
+  rotation: '+=90'
 });
 tl.to('.square', {
   duration: 1.5,
   ease: 'power1.inOut',
   left: '10%',
   top: '10%',
-  rotation: 180
+  rotation: '-=90'
 });
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 console.log('Hello World from main.js!');
 
+gsap.to('.square', { duration: 2, delay: 10, color: 'white' });
+
 let tl = gsap.timeline({
 	repeat: -1,
 	onRepeat() {
-		if (tl.timeScale() < 10) tl.timeScale(tl.timeScale() * 1.2);
+		if (tl.timeScale() < 30) tl.timeScale(tl.timeScale() * 1.2);
 	},
 });
-tl.to('.square', { duration: 1.5, ease: 'power1.inOut', left: '40%', top: '58%', rotation: 90 });
-tl.to('.square', { duration: 1.5, ease: 'power1.inOut', left: '40%', top: '10%', rotation: 180 });
-tl.to('.square', { duration: 1.5, ease: 'power1.inOut', left: '10%', top: '58%', rotation: 270 });
-tl.to('.square', { duration: 1.5, ease: 'power1.inOut', left: '10%', top: '10%', rotation: 180 });
+tl.to('.square', { duration: 1.5, ease: 'power1.inOut', left: '40%', top: '58%', rotation: '+=90' });
+tl.to('.square', { duration: 1.5, ease: 'power1.inOut', left: '40%', top: '10%', rotation: '+=90' });
+tl.to('.square', { duration: 1.5, ease: 'power1.inOut', left: '10%', top: '58%', rotation: '+=90' });
+tl.to('.square', { duration: 1.5, ease: 'power1.inOut', left: '10%', top: '10%', rotation: '-=90' });
