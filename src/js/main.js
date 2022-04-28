1fd7e4cc9acbfe7319c6ffe0d9411e83b59532e4
max a <maxambrogio1@gmail.com>
2022-04-28 16:53:01 -0400

    commit #2

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 console.log('Hello World from main.js!');
 
 gsap.from('.background', { duration: 2, y: '-=5%' });
-gsap.from('.sand-back', { duration: 3, y: '+=2%' });
-gsap.from('.sand-front', { duration: 3, y: '+=5%' });
 gsap.from('.character', { duration: 3, y: '+=8%' });
 
-gsap.from('.text', { delay: 1, duration: 2, opacity: 0, y: '-=5%' });
+gsap.from('.text-1', { delay: 1, duration: 2, opacity: 0, y: '-=5%' });
+gsap.from('.text-2', { delay: 2, duration: 2, opacity: 0, y: '-=5%' });
+gsap.from('.logo', { delay: 3, duration: 2, opacity: 0, y: '-=5%' });
 
 /*
 ideas for pushing it further
